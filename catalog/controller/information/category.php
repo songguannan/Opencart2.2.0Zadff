2af1b676a578d982b9cfdeb6b668448b07b9ddb3
songguannan <405038777@qq.com>
2016-11-20 18:50:22 +0800

    first_commit

---
+++ catalog/controller/information/category.php
@@ -0,0 +1,86 @@
+<?php
+class ControllerInformationCategory extends Controller {
+	public function index() {
+		$data['breadcrumbs'] 	= array();
+		$data['breadcrumbs'][]  = array(
+			'text' => $this->language->get('text_home'),
+			'href' => $this->language->get('common/home')
+		);
+		if (isset($this->request->get['path'])) {
+			$url = '';
+
+			if (isset($this->request->get['sort'])) {
+				$url .= '&sort=' . $this->request->get['sort'];
+			}
+
+			if (isset($this->request->get['order'])) {
+				$url .= '&order=' . $this->request->get['order'];
+			}
+
+			if (isset($this->request->get['limit'])) {
+				$url .= '&limit=' . $this->request->get['limit'];
+			}
+
+			$path = '';
+
+			$parts = explode('_', (string)$this->request->get['path']);
+			//$category_id = (int)array_pop($parts);
+
+			foreach ($parts as $path_id) {
+				if (!$path) {
+					$path = (int)$path_id;
+					
+				} else {
+					$path .= '_' . (int)$path_id;
+
+				}
+				$this->load->model('catalog/category');
+				$category_info = $this->model_catalog_category->getCategory($path_id);
+
+				if ($category_info) {
+					$data['breadcrumbs'][] = array(
+						'text' => $category_info['name'],
+						'href' => $this->url->link('product/category', 'path=' . $path . $url)
+					);
+				}
+			}
+		} else {
+			$category_id = 0;
+		}
+		//读取栏目信息
+		$this->load->model('catalog/information');
+		
+		$data['informations'] = array();
+		foreach ($this->model_catalog_information->getInformations() as $result) {
+				/*//测试代码
+				foreach ($result as $key => $value) {
+					echo $key.'————>'.$value.'<br>';
+				}
+				//测试代码结束*/
+				$soImages="/<img (.*?)>/";/*"/<img (.*?) src=\"(.+?)\".*?>/" */
+				$str=htmlspecialchars_decode($result['description']);
+				preg_match($soImages, $str, $ereg);
+				if(isset($ereg[0])){
+					$thumimg=$ereg[0];
+				}else{
+					$thumimg='';
+				}
+				$data['informations'][] = array(
+					'title' 		=> $result['title'],
+					'href'  		=> $this->url->link('information/information', 'information_id=' . $result['information_id']),
+					'description'   => mb_substr(strip_tags(htmlspecialchars_decode($result['description'])), 0,170,'UTF-8').'...',
+					'thumimg'		=> $thumimg,
+					'createtime' => $result['createtime']
+				);
+			
+		}//information
+			$data['column_left'] = $this->load->controller('common/column_left');
+            $data['column_right'] = $this->load->controller('common/column_right');
+            $data['content_top'] = $this->load->controller('common/content_top');
+            $data['content_bottom'] = $this->load->controller('common/content_bottom');
+            $data['footer'] = $this->load->controller('common/footer');
+            $data['header'] = $this->load->controller('common/header');
+
+		$this->response->setOutput($this->load->view('information/category', $data));
+	}//index()
+}//class
---
+++ catalog/view/theme/default/template/information/category.tpl
@@ -0,0 +1,69 @@
+<?php echo $header; ?>
+<div class="container">
+	<ul class="breadcrumb">
+		<?php foreach ($breadcrumbs as $breadcrumb) { ?>
+			<li><a href="<?php echo $breadcrumb['href'];?>"><?php echo $breadcrumb['text']; ?></a>
+			</li>
+		<?php } ?>
+	</ul>
+		<div class="row">
+			<?php if ($informations) { ?>
+		        <div class="col-sm-12">
+		          	<?php foreach ($informations as $information) { ?>
+		          		<?php if ($information['thumimg']) { ?>
+			          		<div class="container">
+							    <div class="row col-md-12 thum-warp">
+					                <div class="col-md-2 left thum-imgwh">
+					                	<a href="<?php echo $information['href']; ?>">
+					                		<?php echo $information['thumimg']; ?>
+					                	</a>
+					                </div>
+						                <div class="col-md-10 right thum-right">
+						                	<div class="panel panel-info thum-border-left-radius thum-border-left-style" >
+												<div class="panel-heading thum-border-left-radius thum-heading">
+												  	<h3 class="panel-title thum-title"><?php echo $information['title']; ?></h3>
+												</div>
+											    <div class="panel-body thum-body">
+											    	<?php echo $information['description']; ?>
+											  	</div>
+											  	<div class="panel-footer thum-footer thum-border-left-radius">
+											  	 	<div class="thum-border-left-radius col-md-5 thum-time">
+												    	<?php echo $information['createtime']; ?>
+												    </div>
+												    <div class="col-md-5 thum-detail">
+												    	<a href="<?php echo $information['href']; ?>">查看详情>></a>
+												    </div>
+												</div>
+											</div>
+										</div>
+							    </div>
+							</div>
+						<?php }else{ ?>
+							<div class="container">
+								<div class="row col-md-12 thum-warp">
+									<div class="panel panel-info">
+										<div class="panel-heading">
+											<h3 class="panel-title thum-title"><?php echo $information['title']; ?></h3>
+										</div>
+									    <div class="panel-body">
+									    	<?php echo $information['description']; ?>
+									    </div>
+										<div class="panel-footer thum-footer thum-border-left-radius">
+									  	 	<div class="thum-border-left-radius col-md-5 thum-time">
+										    	<?php echo $information['createtime']; ?>
+										    </div>
+										    <div class="col-md-5 thum-detail">
+										    	<a href="<?php echo $information['href']; ?>">查看详情>></a>
+										    </div>
+										</div>
+									</div>
+								</div>
+							</div>
+
+						<?php } ?>
+		          	<?php } ?>
+		        </div>
+		    <?php } ?>
+		</div>
+</div>
+<?php echo $footer; ?>
